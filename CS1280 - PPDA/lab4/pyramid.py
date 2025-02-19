3f4c229fa6558536e602eaac5afe80f695ccdc8f
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-02-19 15:42:36 +0530

    Update pyramid.py

--- CS1280 - PPDA/lab4/pyramid.py
+++ CS1280 - PPDA/lab4/pyramid.py
@@ -8,3 +8,14 @@ for i in range(n):
      print("\n")
      d=d-1
      a=a+2
+
+d=0
+a=a-2
+
+for i in range (n,0,-1):
+     print("  "*d,end=" ")
+     for k in range(a,0,-1):
+          print("*",end=" ")
+     print("\n")
+     d=d+1
+     a=a-2
