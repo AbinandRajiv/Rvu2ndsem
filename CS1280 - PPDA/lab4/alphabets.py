c0d59a7c77867f632d2880e8af5fc5244b34afcb
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-02-19 15:42:21 +0530

    Create alphabets.py

---
+++ CS1280 - PPDA/lab4/alphabets.py
@@ -0,0 +1,14 @@
+a=65
+c=66
+for i in range(4):
+     for j in range(65,c):
+          print(chr(j),end="")
+     c+=2
+     print("\n")
+c-=4
+
+for i in range(3):
+     for j in range(a,c):
+          print(chr(j),end="")
+     c-=2
+     print("\n")
