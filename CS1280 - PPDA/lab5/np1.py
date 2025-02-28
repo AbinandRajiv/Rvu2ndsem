f3f12d217f89c961fa1fe12769fb7774a3ca513b
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-02-28 10:08:51 +0530

    Add files via upload

---
+++ CS1280 - PPDA/lab5/np1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+dim=[[1,2,3,4],[5,6,7,8]]
+arr2 = np.array(dim)
+
+print(" Dimensions: ", arr2.ndim)
+print(" shape: ",arr2.shape)
+print( arr2[1])
+
---
+++ CS1280 - PPDA/lab5/np2.py
@@ -0,0 +1,9 @@
+import numpy as np
+data1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+data2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+data3 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("Shape of 1Dim array:", data1.shape)
+print("Shape of 2Dim array:", data2.shape)
+print("Shape of 3Dim array:", data3.shape)
+
+
