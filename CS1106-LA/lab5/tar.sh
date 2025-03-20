84e6684dfc4e9763e218a965f380ea1f8e0dd64c
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-03-20 10:20:54 +0530

    Delete CS1106-LA/lab5/tar.sh

--- CS1106-LA/lab5/tar.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/Documents/Backup
-cp ~/Documents/*.c ~/Documents/Backup/
-cd ~/Documents
-tar -czf Backup.tar.gz Backup
-rm -rf ~/Documents/Backup/
-echo "Backup process completed successfully"
