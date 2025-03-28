dd5a25df88dd10819a76c7e70646bfab0b24ec2f
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-03-28 09:45:46 +0530

    Create Act4.sh

---
+++ CS1106-LA/lab6/Act4.sh
@@ -0,0 +1,9 @@
+sudo mkdir /tmp/Backups
+cp *.c /tmp/Backups
+cp *.py /tmp/Backups
+cd /tmp
+tar -czvf Backup-2025-03-28.tar.gz
+udisksctl mount -b /dev/sda1
+mv /tmp/Backup-2025-03-28.tar.gz /media/RVU/Pendrive/
+udisksctl unmount -b /dev/sda1
+rmdir /tmp/Backups
