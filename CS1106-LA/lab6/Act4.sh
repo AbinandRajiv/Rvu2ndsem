79fe9f8271b9706c8e976cc17d0df81303c79175
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-03-28 09:49:40 +0530

    Update Act4.sh

--- CS1106-LA/lab6/Act4.sh
+++ CS1106-LA/lab6/Act4.sh
@@ -6,4 +6,4 @@ tar -czvf Backup-2025-03-28.tar.gz
 udisksctl mount -b /dev/sda1
 mv /tmp/Backup-2025-03-28.tar.gz /media/RVU/Pendrive/
 udisksctl unmount -b /dev/sda1
-rmdir /tmp/Backups
+rm -r /tmp/Backups
