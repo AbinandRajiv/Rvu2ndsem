9f2e369493db211780be7ae5887bf8c2c81f2084
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-04-07 22:09:39 +0530

    Delete CS1106-LA/lab8/set1 directory

--- CS1106-LA/lab8/set1/backup.sh
+++
@@ -1,8 +0,0 @@
-echo "Username: "
-echo "$USERNAME"
-echo "Home directory contents : "
-ls ~/
-echo "Shell : "
-echo "$SHELL"
-echo "Group memberships : "
-groups
--- CS1106-LA/lab8/set1/logarchive.sh
+++
@@ -1,8 +0,0 @@
-sudo mkdir /var/log/backups
-
-cd /var/log
-
-sudo cp *.log backups
-sudo tar -czf backup_YYYYMMDD.tar.gz backups
-sudo tar -tvf backup_YYYYMMDD.tar.gz
-
